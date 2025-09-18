85edcb51b146aa6f83ef80a4e7d3868d8c433226
intanNanda <intan.irnanda@sera.astra.co.id>
2025-09-18 22:04:27 +0700

    fixed router

--- react-router.config.ts
+++ react-router.config.ts
@@ -3,5 +3,5 @@ import type { Config } from "@react-router/dev/config";
 export default {
   ssr: false,
   prerender: false,
-  basename: process.env.NODE_ENV === 'production' ? '/release-note-manual' : undefined,
+  basename: process.env.NODE_ENV === 'production' ? '/release-note-manual/' : undefined,
 } satisfies Config;
